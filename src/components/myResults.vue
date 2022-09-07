1e7bbea815d85c46f0932f24ea73d0bf664adb93
Marco Valerio <mo.valerio93@gmal.com>
2022-09-07 13:26:09 +0200

    post done

--- src/components/myResults.vue
+++ src/components/myResults.vue
@@ -4,7 +4,10 @@
           <h2> REAL WORLD RESULTS</h2>
           <img class="divider-xx" src='../assets/images/divider-xx-red.png' alt="">
       </div>
-      
+      <div class="post" v-for="(post, index) in posts" :key="index">
+        <p>{{post.text}}</p>
+        <p>{{post.name}}</p>
+      </div>
       
   </div>
 </template>
@@ -17,19 +20,27 @@ export default {
     data() {
       return{
         posts:[
-          {            
+          { 
+            img:'',           
             text:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat quo nesciunt dolorum ipsum officiis itaque ad. Beatae illum recusandae nesciunt, quae, aliquid natus nam corrupti, minus incidunt quas quo ipsa!',
             name:'-Tara Smith'
           },
-          {            
+          { 
+            img:'',             
             text:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat quo nesciunt dolorum ipsum officiis itaque ad. Beatae illum recusandae nesciunt, quae, aliquid natus nam corrupti, minus incidunt quas quo ipsa!',
             name:'-Tara Smith'
           },
-          {            
+          {  
+            img:'',            
             text:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat quo nesciunt dolorum ipsum officiis itaque ad. Beatae illum recusandae nesciunt, quae, aliquid natus nam corrupti, minus incidunt quas quo ipsa!',
             name:'-Tara Smith'
           },
-        ]
+          {  
+            img:'',            
+            text:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat quo nesciunt dolorum ipsum officiis itaque ad. Beatae illum recusandae nesciunt, quae, aliquid natus nam corrupti, minus incidunt quas quo ipsa!',
+            name:'-Tara Smith'
+          },
+        ],
       }
     }
     
